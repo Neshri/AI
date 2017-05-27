9251b0d5ba5e03dddc652fc5eb04f869728668e8
[neshri] <[anton.lundgren94@gmail.com]>
2017-05-27 05:56:20 +0200

    Added names to nodes for easement of training

---
+++ FeedForward/src/network/Variable.java
@@ -0,0 +1,11 @@
+package network;
+
+public class Variable {
+
+	public final String name;
+	public final double value;
+	public Variable(String name, double value) {
+		this.name = name;
+		this.value = value;
+	}
+}
